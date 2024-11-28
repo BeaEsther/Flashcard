fa244635c809eb0af3b7020af4078dea942127d1
BeaEsther <00001097162874sp@al.educacao.sp.gov.br>
2024-11-27 23:50:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'Qual é músculo mais potente do corpo humano?',
+    'Massete'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o osso mais pequeno do corpo?',
+    'O estribo'
+)
+
+criaCartao(
+    'fisica',
+    'Qual é a velocidade escalar média, em km/h, de uma pessoa que percorre a pé 1200 m em 20 min?',
+    '3,6'
+)
+
+criaCartao(
+    'Português',
+    'Qual ano ocorreu a primeira fase do romantismo no Brasil?',
+    'ocorreu entre 1836 e 1852 '
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é outro sinônimo de momento linear?',
+    'Quantidade de movimento'
+)
+
+criaCartao(
+    'Inglês',
+    'o que é verb to be',
+    'é um verbo irregular da língua inglesa que significa "ser" ou "estar"'
+)
